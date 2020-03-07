7337f61016bc3a6b055b57de2a2f9ddde04a01e4
PolinaSheverdyaeva” <sheverdyaeva@gmail.com>
2020-03-08 00:03:05 +1100

    homework 4

--- src/test/java/definitions/JavaStepDefs.java
+++ src/test/java/definitions/JavaStepDefs.java
@@ -2,20 +2,23 @@ package definitions;
 
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+
+import java.util.Collections;
 
 public class JavaStepDefs {
     @Given("I say hello world")
     public void iSayHelloWorld() {
-         System.out.println("Hello World!");
+        System.out.println("Hello World!");
 
-         String var1;
-          var1 = "Hello World!";
-          System.out.println(var1);
+        String var1;
+        var1 = "Hello World!";
+        System.out.println(var1);
 
-          String firstName = "Slava";
-          String lastName = "Skryabin";
+        String firstName = "Slava";
+        String lastName = "Skryabin";
 
-        System.out.println("Full name: "+ firstName + " " + lastName);
+        System.out.println("Full name: " + firstName + " " + lastName);
         System.out.println(firstName.toUpperCase());
         System.out.println(firstName.length());
 
@@ -29,8 +32,12 @@ public class JavaStepDefs {
 
     }
 
-    @Given("I performe actions with {string} and {string}")
-    public void iPerformeActionsWithAnd(String str1, String str2) {
+    @Given("I perform actions with {string} and {string}")
+    public void iPerformActionsWithAnd(String str1, String str2) {
+
+        System.out.println(this);
+
+//        Collections.copy(null, null);
 
         System.out.println("str1: " + str1);
         System.out.println("str1: " + str2);
@@ -52,6 +59,78 @@ public class JavaStepDefs {
         int j = 5;
         System.out.println("var i = " + j);
 //        double d =3;
-        System.out.println(10.0/3);
+        System.out.println(10.0 / 3);
+    }
+
+    @Given("I print url for {string} page")
+    public void iPrintUrlForPage(String site) {
+
+
+        if (site.equalsIgnoreCase("google")) {
+            System.out.println("https://www.google.com/");
+        } else if (site.equalsIgnoreCase("sample")) {
+            System.out.println("https://skryabin.com/webdriver/html/sample.html");
+//        } else
+//            {
+//            System.out.println("Not supported site! Actual: " + site);
+        }
+    }
+
+    @And("I {string}")
+    public void iTypeMyText(String myText) {
+
+        System.out.println(myText);
+    }
+
+
+    @When("I count to {string}")
+    public void iCountTo(String limit) {
+
+        int lim = Integer.valueOf(limit); //conversion of string to integer
+        for (int counter = 1; counter <= lim; counter++)
+            System.out.printf("%d ", counter);
+
+        System.out.println();
+    }
+
+    @When("I call you {string}")
+    public void iCallYour(String name)
+    {
+        switch(name)
+        {
+            case "Polina":
+                System.out.println("Hello, Polina!:)");
+                break;
+            case "Katya":
+                System.out.println("Hello, Katya!:)");
+                break;
+                default:
+                    throw new RuntimeException("I don't know you, " + name + "!");
+        }
+    }
+
+    @Given("I perform actions with  numbers {string} and {string}")
+    public void iPerformActionsWithNumbersAnd(String number1, String number2) {
+
+        int a = Integer.valueOf(number1); //conversion of string to integer
+        int b = Integer.valueOf(number2);
+
+        if (a == b) // declare an if statement to determine if num1 and num2 equal
+            System.out.printf( "%d == %d\n", a, b );
+
+        if (a != b)
+            System.out.printf( "%d != %d\n", a, b );
+
+        if (a >= b)
+            System.out.printf( "%d >= %d\n", a, b );
+
+        if (a <= b)
+            System.out.printf( "%d <= %d\n", a, b );
+
+        if (a > b)
+            System.out.printf( "%d > %d\n", a, b );
+
+        else if (a > b)
+        System.out.printf( "%d > %d\n", a, b );
     }
 }
--- src/test/resources/features/java.feature
+++ src/test/resources/features/java.feature
@@ -6,5 +6,10 @@
       Given I say hello world
       And I say "hello again"
       When I say "my message"
-      Given I performe actions with "hello world" and "hello w"
+      Given I perform actions with "hello world" and "hello w"
+      Given I print url for "yahoo" page
+      And I "I have finished home work 4!"
+      When I count to "100"
+      When I call you "Polina"
+      Given I perform actions with  numbers "10" and "5"
 
